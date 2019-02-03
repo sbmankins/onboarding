4b1811390dbc71c9a348563ed872f1ffdccd9e58
Steve M <smankins@msn.com>
2019-02-02 20:27:29 -0600

    update app bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,43 +1,27 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import {Link} from 'react-router-dom';
 
-const styles = theme => ({
-  button: {
-    margin: theme.spacing.unit,
-  },
-  input: {
-    display: 'none',
-  },
-});
 
-const NavBar = (props) => {
-  const { classes } = props;
+const NavBar = () => {
+
     return(
         <div>
-        <Grid container direction="row" justify="space-between" alignItems="flex-start" spacing={8}>
-        <AppBar position="static">
-            <Toolbar>
-             <Grid item xs={12} sm={12} md={12} lg={12}>
-                <Typography variant="title" color="inherit">Bayer IT Onboarding</Typography>
+          <Grid container direction="row" justify="space-between" alignItems="flex-start" spacing={8}>
+            <AppBar position="static">
+              <Toolbar>
+                <Grid item xs={12} sm={12} md={12} lg={12}>
+                  <Link style={{ textDecoration: 'none', color: 'white' }} to={'/'}><Typography variant="title" color="inherit">Bayer IT Onboarding</Typography></Link>
                 </Grid>
-                <Grid item xs={3} sm={2} md={2} lg={2}>
-                <Button variant="contained" className={classes.button} color="gray" href="/"><Typography variant="button">Home</Typography></Button>
-                </Grid>
-            </Toolbar>
-        </AppBar>
-        </Grid>
+              </Toolbar>
+            </AppBar>
+          </Grid>
         </div>
     )
 }
 
-NavBar.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
-export default withStyles(styles)(NavBar);
+export default NavBar;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
+
 import App from './components/App';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(() => [], {}, applyMiddleware(reduxThunk));
 
 ReactDom.render
 (
