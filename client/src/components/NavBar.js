36317799530d1ca4ff0b4f728dce8a91b35d08a9
Steve M <smankins@msn.com>
2019-02-04 09:20:48 -0600

    add an input field to form

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = (theme) => ({
-  toolbar: theme.mixins.toolbar,
+  toolbar: theme.mixins.toolbar
 });
 
 const NavBar = (props) => {
@@ -15,8 +15,9 @@ const NavBar = (props) => {
   return (
         <div>
           <AppBar>
-            <Toolbar>
+            <Toolbar style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
               <Link style={{ textDecoration: 'none', color: 'white' }} to={'/'}><Typography variant="title" color="inherit">Bayer IT Onboarding</Typography></Link>
+              <Link style={{ textDecoration: 'none', color: 'white' }} to={'/new'}><Typography variant="button" color="inherit">New</Typography></Link>
             </Toolbar>
           </AppBar>
           <div className={classes.toolbar}/>
--- client/src/components/forms/EmployeeForm.js
+++ client/src/components/forms/EmployeeForm.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
 import {reduxForm, Field} from 'redux-form';
+import FormField from './FormField';
 
 class EmployeeForm extends Component{
+
+  renderFields(){
+    return(
+      <div>
+        <Field
+          type="text"
+          name="surveyTitle"
+          component={FormField}
+        />
+      </div>
+    );
+  }
+
   render(){
     return(
       <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-        <Field
-          type="text"
-          name="formTitle"
-          component="input"
-        />
+        <FormField />
         <button type="submit">submit</button>
       </form>
     );
