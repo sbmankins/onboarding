4ccf216b638c6e4ec6514328a5bddae144647894
Steve M <smankins@msn.com>
2019-04-01 11:14:53 -0500

    add slack scheduling

---
+++ daysBetween.js
@@ -0,0 +1,18 @@
+module.exports = {
+    daysBetween: function(start) {
+        //Calculates the numbers of days between two dates
+        const oneDay = 24 * 60 * 60 * 1000;
+        start = new Date(start).toLocaleDateString('en-US', {
+            timeZone: 'UTC',
+        });
+        const now = new Date();
+        const startDate = new Date(start);
+        startDate.setHours(0, 0, 0, 0);
+        now.setHours(0, 0, 0, 0);
+
+        const diffDays = Math.round(
+            (startDate.getTime() - now.getTime()) / oneDay
+        );
+        return diffDays;
+    },
+};
--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
+const daysBetween = require('./daysBetween');
+const { WebClient } = require('@slack/client');
+const schedule = require('node-schedule');
+
+const token = keys.slackAPI;
+const conversationID = keys.conversationID;
+const web = new WebClient(token);
+
+var rule = new schedule.RecurrenceRule();
+rule.hour = 5;
 
 require('./models/Employee');
 require('./models/Admin');
@@ -43,6 +53,62 @@ app.get('/', (req, res) => {
 });
 
 require('./routes/employeeRoutes')(app);
+
+const Employee = mongoose.model('employees');
+
+// Used to determine what daily messages to post to slack based on employee start date - used for scheduled messages
+const job = async function fetchEmployees() {
+    const empl = await Employee.find()
+        .populate('_admin')
+        .populate('_manager')
+        .populate('_status')
+        .populate('_role')
+        .populate('_vendor')
+        .populate('_type')
+        .populate('_region')
+        .populate('_team')
+        .populate('_hirestatus')
+        .populate('_campus')
+        .populate('_leader')
+        .populate('_platform')
+        .populate('_computer')
+        .sort({
+            dateStart: 'ascending',
+            _status: 'ascending',
+            lastName: 'ascending',
+            _manager: 'ascending',
+            _admin: 'ascending',
+        })
+        .exec();
+
+    const result = await empl.filter(
+        employee => employee._status.name !== 'Complete'
+    );
+    await result.forEach(employee => {
+        let start = new Date();
+        start = employee.dateStart;
+        let dayString;
+        let days = daysBetween.daysBetween(start);
+        if (days <= 7) {
+            days <= 0 ? (days = 0) : (days = days);
+            days === 1 ? (dayString = 'day') : (dayString = 'days');
+            web.chat.postMessage({
+                channel: conversationID,
+                username: 'Onboarding Bot',
+                text: `${employee.firstName} ${
+                    employee.lastName
+                } starts in ${days} ${dayString} and is not marked complete.
+              Manager: ${employee._manager.name}
+              Admin: ${employee._admin.name}`,
+            });
+        }
+    });
+    console.log('daily task ran');
+    return result;
+};
+
+//Runs fetchEmployees at the time specified in the rule
+schedule.scheduleJob(rule, job);
 
 if (process.env.NODE_ENV === 'production') {
     //Express will serve main.js, main.css, etc
