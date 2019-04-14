e71f69cecc8a8f766918b9f209f941f0cb7c942a
sbmankins <smankins@msn.com>
2019-04-13 20:49:12 -0500

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,8 @@
 //dev.js don't commit
 module.exports = {
     mongoURI:
-        'mongodb://smankins:usaf2020@ds153495.mlab.com:53495/onboardingdb',
+        'YOUR STRING HERE',
     slackAPI:
-        'xoxp-556711478549-557698478503-587237893136-74fdfec3cb4619b18fbb703d7b166f56',
-    conversationID: 'CGCA2H292',
+        'YOUR STRING HERE',
+    conversationID: 'YOUR STRING HERE',
 };
